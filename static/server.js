d33ad74eb7d0152af3f6cddba7d237d74ea0de8b
Kyle DiPeppe <kyle.dipeppe@basketsavings.com>
2020-05-18 19:27:27 -0400

    changed app.get in server.js

--- static/server.js
+++ static/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
   });
 
 app.listen(port, () => console.log("Listening on Port", port)) 
