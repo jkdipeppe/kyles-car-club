21415b49612e944326ffe1a08b87add16c394ca9
Kyle DiPeppe <kyle.dipeppe@basketsavings.com>
2020-05-18 18:48:03 -0400

    pointed to public and added .get in server.js

--- static/server.js
+++ static/server.js
@@ -3,6 +3,9 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 
-app.use('/', express.static(path.join(__dirname, 'src')))
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 app.listen(port, () => console.log("Listening on Port", port)) 
