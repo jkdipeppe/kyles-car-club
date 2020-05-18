a0b20be7dfb7f60a097da5b6fd20c2bbc024ac49
Kyle DiPeppe <kyle.dipeppe@basketsavings.com>
2020-05-18 18:42:54 -0400

    pointed to src folder

--- static/server.js
+++ static/server.js
@@ -3,6 +3,6 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'src')))
 
 app.listen(port, () => console.log("Listening on Port", port)) 
